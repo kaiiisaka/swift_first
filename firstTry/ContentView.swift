3d177628ec64f0a4686806153390a43752a7d7d3
Александр Мельников <alex@MacBook-Pro-Aleksandr.local>
2023-08-06 15:27:40 +0300

    initial

---
+++ firstTry/ContentView.swift
@@ -0,0 +1,31 @@
+//
+//  ContentView.swift
+//  firstTry
+//
+//  Created by Александр Мельников on 05.08.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Toggle(isOn: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Is On@*/.constant(true)/*@END_MENU_TOKEN@*/) {
+                /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Label@*/Text("Label")/*@END_MENU_TOKEN@*/
+            }
+            DatePicker(selection: /*@START_MENU_TOKEN@*/.constant(Date())/*@END_MENU_TOKEN@*/, label: { /*@START_MENU_TOKEN@*/Text("Date")/*@END_MENU_TOKEN@*/ })
+            Image(systemName:"globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+            .previewDevice("iPhone 14")
+    }
+}
---
+++ firstTry/firstTryApp.swift
@@ -0,0 +1,17 @@
+//
+//  firstTryApp.swift
+//  firstTry
+//
+//  Created by Александр Мельников on 05.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct firstTryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
